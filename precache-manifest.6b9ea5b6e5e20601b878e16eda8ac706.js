9d8a648c61c540b924b987b65e1cfca23ecb02d0
Faysal AlZoubi <faysal.zoabi@gmail.com>
2018-12-31 12:27:08 +0100

    Updates

---
+++ precache-manifest.6b9ea5b6e5e20601b878e16eda8ac706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/WeatherApp/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "e780dca97e9f3ac65f8d",
+    "url": "/WeatherApp/static/js/main.e780dca9.chunk.js"
+  },
+  {
+    "revision": "0b939a754185965dee45",
+    "url": "/WeatherApp/static/js/1.0b939a75.chunk.js"
+  },
+  {
+    "revision": "e780dca97e9f3ac65f8d",
+    "url": "/WeatherApp/static/css/main.3be6a858.chunk.css"
+  },
+  {
+    "revision": "ccbcdaf96335534ba7cd4122b71a1840",
+    "url": "/WeatherApp/index.html"
+  }
+];
